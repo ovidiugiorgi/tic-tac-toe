008fb4f2db6a4c11efa577b13b7c148e243ec608
Ovidiu Giorgi <ovidiugiorgi@gmail.com>
2017-10-22 17:49:26 +0300

    Use two loops when rendering squares in Board

--- src/components/Board.js
+++ src/components/Board.js
@@ -12,23 +12,19 @@ export default class Board extends React.Component {
   }
 
   render() {
+    const squares = [];
+    for (let row = 0; row < 3; row++) {
+      const squaresRow = [];
+      for (let col = 0; col < 3; col++) {
+        const index = row * 3 + col;
+        squaresRow.push(this.renderSquare(index));
+      }
+      squares.push(<div className="board-row"> {squaresRow} </div>)
+    }
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+      {squares}
       </div>
     );
   }
